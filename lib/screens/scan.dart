1a3072776bac944800b0c817ec388084e0c12b17
Shaunak <shaunak.warty@gmail.com>
2020-11-04 18:25:01 -0800

    Add scanner and buttons to toggle camera and flash

--- lib/screens/scan.dart
+++ lib/screens/scan.dart
@@ -1,14 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-class ScanScreen extends StatelessWidget {
+class ScanScreen extends StatefulWidget {
+  @override
+  _ScanScreenState createState() => _ScanScreenState();
+}
+
+class _ScanScreenState extends State<ScanScreen> {
+  final Key _qrKey = GlobalKey(debugLabel: 'QR');
+
+  QRViewController _controller;
+  bool _flash = false;
+
+  void _onScanned(String data) {
+    print(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Scan QR Code'),
       ),
-      body: Center(
-        child: Text('Scan'),
+      body: Stack(
+        children: [
+          QRView(
+            key: _qrKey,
+            onQRViewCreated: (controller) {
+              setState(() {
+                _controller = controller;
+              });
+              controller.scannedDataStream.distinct().listen(_onScanned);
+            },
+            overlay: QrScannerOverlayShape(
+              borderWidth: 5.0,
+              borderRadius: 5.0,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(
+                icon: Icon(_flash ? Icons.flash_on : Icons.flash_off),
+                onPressed: _controller != null ? () {
+                  _controller.toggleFlash();
+                  setState(() {
+                    _flash = !_flash;
+                  });
+                } : null,
+              ),
+              IconButton(
+                icon: Icon(Icons.flip_camera_android),
+                onPressed: _controller?.flipCamera,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
