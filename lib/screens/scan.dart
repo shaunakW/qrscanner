fe67cf5406c152b2197c1f7a143046758778af8c
Shaunak <shaunak.warty@gmail.com>
2020-11-04 17:25:00 -0800

    Create single empty screen

---
+++ lib/screens/scan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScanScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Scan QR Code'),
+      ),
+      body: Center(
+        child: Text('Scan'),
+      ),
+    );
+  }
+}
